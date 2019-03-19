685e987f85d954f2cf58a1d3fbe9b1f451fd6940
Mateusz Swakon <swakon.mateusz@gmail.com>
2019-03-20 00:47:24 +0100

    RequestMapping

--- pharmacy/src/main/java/pl/swakon/pharmacy/chemicals.java
+++ pharmacy/src/main/java/pl/swakon/pharmacy/chemicals.java
@@ -1,7 +1,46 @@
 package pl.swakon.pharmacy;
 
+
 import java.time.LocalDate;
 
 public class chemicals {
+    private Long id;
+    private String title;
+    private LocalDate ExpirationDate;
 
+    public chemicals() { }
+
+
+    public chemicals(Long id, String title, LocalDate ExpirationDate) {
+        this.id = id;
+        this.title = title;
+        this.ExpirationDate = ExpirationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDate getExpirationDate() {
+        return ExpirationDate;
+    }
+
+    public void setexpirationDate(LocalDate expirationDate) {
+        this.ExpirationDate = ExpirationDate;
+    }
 }
+
+
+
--- pharmacy/src/main/java/pl/swakon/pharmacy/chemicalsApp.java
+++ pharmacy/src/main/java/pl/swakon/pharmacy/chemicalsApp.java
@@ -1,16 +1,36 @@
 package pl.swakon.pharmacy;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+
+@RestController
+@RequestMapping("/app/chemicals/")
 public class chemicalsApp {
 
     @GetMapping("/status")
     public String  getStatus(){
 
-        return "App dziala";
+        return "App working";
+    }
+    private List<chemicals> chemicals;
+
+    public chemicalsApp() {
+        chemicals = new ArrayList<>();
+        chemicals.add(new chemicals(1L, "Apap", LocalDate.of(2034, 1, 1)));
+        chemicals.add(new chemicals(2L, "Nospa", LocalDate.of(2023, 2, 2)));
+        chemicals.add(new chemicals(3L, "Ranigast", LocalDate.of(2021, 2, 3)));
+        chemicals.add(new chemicals(4L, "RanigastMax", LocalDate.of(2020, 2, 4)));
     }
 
+    @GetMapping("/all")
+    public List<chemicals> getAll() {
+        return chemicals;
+    }
 }
