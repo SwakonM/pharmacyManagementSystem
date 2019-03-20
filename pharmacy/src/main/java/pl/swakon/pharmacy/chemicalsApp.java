105e5a9074cc609e59c59a7bae8311276c13becb
Mateusz Swakon <swakon.mateusz@gmail.com>
2019-03-20 07:51:23 +0100

    DeleteMapping

--- pharmacy/src/main/java/pl/swakon/pharmacy/chemicalsApp.java
+++ pharmacy/src/main/java/pl/swakon/pharmacy/chemicalsApp.java
@@ -1,24 +1,23 @@
 package pl.swakon.pharmacy;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
-@RequestMapping("/app/chemicals/")
+@RequestMapping("/app/chemicals")
 public class chemicalsApp {
 
     @GetMapping("/status")
-    public String  getStatus(){
+    public String getStatus() {
 
         return "App working";
     }
+
     private List<chemicals> chemicals;
 
     public chemicalsApp() {
@@ -29,8 +28,22 @@ public class chemicalsApp {
         chemicals.add(new chemicals(4L, "RanigastMax", LocalDate.of(2020, 2, 4)));
     }
 
+
     @GetMapping("/all")
     public List<chemicals> getAll() {
         return chemicals;
     }
+
+    @GetMapping
+    public chemicals getById(@RequestParam int index) {
+        Optional<chemicals> first = chemicals.stream().
+                filter(element -> element.getId() == index).findFirst();
+        return first.get();
+    }
+
+    @DeleteMapping // usuwa element
+    public boolean deleteChemicals(@RequestParam int index) {
+        return chemicals.removeIf(element -> element.getId() == index);
+    }
 }
+
